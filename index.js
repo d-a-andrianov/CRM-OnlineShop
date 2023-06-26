a522199d85db5de6f352828fbf0e3308937c8d0e
Дмитрий Андрианов <d.a.andrianow@gmail.com>
2023-06-26 22:13:09 +0300

    подключаем JS

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const productName = 'Навигационная система Soundmax';
+const productUnits = '5';
+const productCategory = 'Техника для дома';
+const productPrice = '100';
+const productCurrency = '$';
+
+const productResult = (productUnits * productPrice);
+
+console.log (productResult + productCurrency);
