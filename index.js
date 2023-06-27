d3698617a1f3f39861c3b0989e46098305a6ac16
Дмитрий Андрианов <d.a.andrianow@gmail.com>
2023-06-27 20:38:49 +0300

    js-4mod

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-'use strict';
+// 'use strict';
 
-const productName = 'Навигационная система Soundmax';
-const productUnits = '5';
-const productCategory = 'Техника для дома';
-const productPrice = '100';
-const productCurrency = '$';
+// const productName = 'Навигационная система Soundmax';
+// const productUnits = '5';
+// const productCategory = 'Техника для дома';
+// const productPrice = '100';
+// const productCurrency = '$';
 
-const productResult = (productUnits * productPrice);
+// const productResult = (productUnits * productPrice);
 
-console.log (productName);
-console.log (productResult + productCurrency);
+// console.log (productName);
+// console.log (productResult + productCurrency);
+
+
+// document.body.innerHTML = 
+// `<h1 class="title">Привет ${productName}</h1>
+// <p>Мне 34 года</p>`;
+
+
+// const myName = 'Дима';
+// console.log (`Меня зовут ${myName}`);
+
+// console.log (`Меня зовут` + myName);
+
+// console.log (2 === 2);
+
+// console.log (Symbol('key1') === Symbol('key1'));
