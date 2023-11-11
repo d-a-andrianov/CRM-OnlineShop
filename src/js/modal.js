776d0b8f895f672cfb70ba633660737614fccc3d
Дмитрий Андрианов <d.a.andrianow@gmail.com>
2023-11-11 17:06:37 +0300

    ДЗ 5.2_на провервку

---
+++ src/js/modal.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Заголовок модального окна
+const modalTitle = document.querySelectorAll('.modal__title');
+console.log(modalTitle);
+
+// Кнопка возле id модального окна
+const modalButtonX = document.querySelectorAll('.modal__button-x');
+console.log(modalButtonX);
+
+// id модального окна
+const modalId = document.querySelectorAll('.modal__id');
+console.log(modalId);
+
+// Форма модального окна
+const modalForm = document.querySelectorAll('.modal__form');
+console.log(modalForm);
+
+// Чекбокс и  Поле рядом с чекбоксом модального окна
+const modalCheckbox = document.querySelectorAll('.modal__checkbox');
+console.log(modalCheckbox); 
+
+// Поле рядом с чекбоксом модального окна
+const modalInputSmall = document.querySelectorAll('.modal__input-small');
+console.log(modalInputSmall); 
+
+// Итоговая стоимость модального окна
+const modalFinalPrice = document.querySelectorAll('.modal__final-price');
+console.log(modalFinalPrice);
+
