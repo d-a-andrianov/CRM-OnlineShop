9c7714abc476d06261cb9cbfa388e889c0374fc2
Дмитрий Андрианов <d.a.andrianow@gmail.com>
2023-11-13 14:21:39 +0300

    ДЗ 5.2_убрал display none и selectorAll

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,30 +1,30 @@
 'use strict';
 
 // Заголовок модального окна
-const modalTitle = document.querySelectorAll('.modal__title');
+const modalTitle = document.querySelector('.modal__title');
 console.log(modalTitle);
 
 // Кнопка возле id модального окна
-const modalButtonX = document.querySelectorAll('.modal__button-x');
+const modalButtonX = document.querySelector('.modal__button-x');
 console.log(modalButtonX);
 
 // id модального окна
-const modalId = document.querySelectorAll('.modal__id');
+const modalId = document.querySelector('.modal__id');
 console.log(modalId);
 
 // Форма модального окна
-const modalForm = document.querySelectorAll('.modal__form');
+const modalForm = document.querySelector('.modal__form');
 console.log(modalForm);
 
 // Чекбокс и  Поле рядом с чекбоксом модального окна
-const modalCheckbox = document.querySelectorAll('.modal__checkbox');
-console.log(modalCheckbox); 
+const modalCheckbox = document.querySelector('.modal__checkbox');
+console.log(modalCheckbox);
 
 // Поле рядом с чекбоксом модального окна
-const modalInputSmall = document.querySelectorAll('.modal__input-small');
-console.log(modalInputSmall); 
+const modalInputSmall = document.querySelector('.modal__input-small');
+console.log(modalInputSmall);
 
 // Итоговая стоимость модального окна
-const modalFinalPrice = document.querySelectorAll('.modal__final-price');
+const modalFinalPrice = document.querySelector('.modal__final-price');
 console.log(modalFinalPrice);
 
